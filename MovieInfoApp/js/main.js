1f6f01f3507201a327a180aeaa0569937c7b0b6a
kafadarova <k.kafadarova@gmail.com>
2018-09-23 19:48:39 +0200

    Add new function to display a signle movie on an other page with more details and possible redirect to the IMDB page

--- MovieInfoApp/js/main.js
+++ MovieInfoApp/js/main.js
@@ -9,6 +9,7 @@ $(document).ready(() => {
   });
 });
 
+// Make request to a API and search for movies and than display the result in the main.html
 function getMovies(searchText) {
   // Make a request to the API with Axios
   axios.get('http://www.omdbapi.com/?apikey=4568cca1&s=' + searchText)
@@ -29,9 +30,63 @@ function getMovies(searchText) {
         </div>
         `;
       });
-      // Change the html code in the div with the ID movies 
+      // Change the html code in the div with the ID movies
       $('#movies').html(output);
     })
+    .catch((err) =>{
+      console.log(err);
+    });
+}
+
+function movieSelected(id){
+  // Clear out when the browser is closed
+  // Store the movie id in the sessionStorage
+  sessionStorage.setItem('movieId', id);
+  // Change location and go to the single movie page
+  window.location = 'movie.html';
+  return false;
+}
+
+function getMovie() {
+  // Retrive the movieId from the sessionStorage
+  let movieId = sessionStorage.getItem('movieId');
+
+  axios.get('http://www.omdbapi.com/?apikey=4568cca1&i=' + movieId)
+  // Return a promise
+    .then((response) => {
+      console.log(response);
+      let movie = response.data;
+
+      let output = `
+      <div class="row">
+        <div class="col-md-4">
+            <img src="${movie.Poster}" class="thumbnail">
+        </div>
+        <div class="col-md-8">
+        <h5>${movie.Title}</h5>
+          <ul class="list-group">
+            <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+            <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+            <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+            <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+            <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+            <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="well">
+        <h3>Plot</h3>
+        ${movie.Plot}
+          <hr>
+        <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+        <a href="index.html" class="btn btn-default">Go Back To Search</a>
+        </div>
+      </div>
+      `;
+
+    $('#movie').html(output);
+        })
     .catch((err) =>{
       console.log(err);
     });
