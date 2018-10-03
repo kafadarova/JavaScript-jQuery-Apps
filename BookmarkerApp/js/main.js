34e17fbc8f9ea68061d3951471fcab2e45855dec
kafadarova <k.kafadarova@gmail.com>
2018-10-03 05:42:15 +0200

    Download bootstrap, update the structure of the project + add a event listener for form submitting

---
+++ BookmarkerApp/js/main.js
@@ -0,0 +1,10 @@
+// Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+function saveBookmark(e) {
+// Ger form values
+let siteName = document.getElementById('siteName');
+
+// Prevent form from submitting
+e.preventDefault();
+}
