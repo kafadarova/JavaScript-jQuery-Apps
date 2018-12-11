079ca7bcad0c35557d00d09fc7461644fbc78fdd
kafadarova <k.kafadarova@gmail.com>
2018-12-11 22:58:04 +0100

    create the track component for the audio player

---
+++ MusicStreamingApp/src/components/Track.js
@@ -0,0 +1,11 @@
+// require React, extract the Component class from React
+import React, {Component} from 'react';
+// requite a few components provided by React SoundPlayer
+import { PlayButton, Progress, Timer } from 'react-soundplayer/components';
+
+// exporting the class - it can be imported into another file
+export default class Track extends Component{
+  render() {
+
+  }
+}
