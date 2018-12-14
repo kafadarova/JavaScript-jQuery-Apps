84ef9b36ef65f6178cc3c38179de170a14c289fc
kafadarova <k.kafadarova@gmail.com>
2018-12-14 23:04:45 +0100

    Import React and SoundPlayerContainer which gives the props to build the audio player

---
+++ MusicStreamingApp/src/components/ProgressSoundPlayer.js
@@ -0,0 +1,17 @@
+import React, {Component, PropTypes} from 'react';
+// higher level container that propagates its children with the props they will need to build the audio player
+import { SoundPlayerContainer } from 'react-soundplayer/addons';
+import Track from './Track';
+
+// create  the ProgressSoundPlayer component
+export default class ProgressSoundPlayer extends Component {
+  render() {
+    const {resolveUrl, clientId} = this.props;
+    return (
+      // pass the resolveUrl and clientId as props
+        <SoundPlayerContainer resolveUrl = {resolveUrl} clientId = {clientId}>
+      <Track />
+    </SoundPlayerContainer>
+    )
+  }
+}
