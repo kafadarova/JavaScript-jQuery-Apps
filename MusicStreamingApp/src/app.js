f9b7b43d9397d5e408be6f4ffc7fb0d73ab2a527
kafadarova <k.kafadarova@gmail.com>
2018-12-15 22:32:54 +0100

    add the soundcloud id

--- MusicStreamingApp/src/app.js
+++ MusicStreamingApp/src/app.js
@@ -4,3 +4,11 @@ import ReactDOM from 'react-dom';
 import ProgressSoundPlayer from './components/ProgressSoundPlayer';
 import SC from 'node-soundcloud';
 import Loading from 'react-loading'
+
+// Add the soundcloud id
+let client_id = 'YOUR SOUNDCLOUD APP ID';
+
+// Initialize the node-souded library
+SC.init({
+  id: client_id
+});
